4ceb1744d03dc66b71c5d6646ab88bad74f8f09b
aceDavon <makojidavid@gmail.com>
2022-10-12 11:58:26 +0100

    create challenge code
    Create and implement the challenge code to fix the problem statement

---
+++ README.md
@@ -0,0 +1,2 @@
+# Unique value counter with multiple pointers
+This repo is part of a group of repos created to practice Algorithm and Data structures with automated workflows and tests
---
+++ challenge.js
@@ -0,0 +1,22 @@
+const uniqueCounter = (arr) => {
+  // check for an empty arr and return 0
+  if(arr.length === 0) return 0;
+
+  // initialize the first pointer
+  let i = 0;
+
+  // initialize the navigator pointer
+  for(let j = 1; j<=arr.length -1; j++) {
+
+    // when a unique item is found, move first pointer one
+    // step up and assign the navigator pointer value to it
+    if(arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    };
+  };
+  // return the position of the first pointer after the loop
+  return i + 1;
+};
+
+console.log(uniqueCounter([1, 2, 2, 3, 4, 5, 7]));
